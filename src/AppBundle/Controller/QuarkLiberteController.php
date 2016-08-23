6f737137e8d9b6a72f25568720b1d0b7c98181b0
Moh-K <mohamed59100.khiter@gmail.com>
2016-08-23 10:39:24 +0200

    Bar du bas (contact, plan, confidentialite, etc..) + controllers

---
+++ src/AppBundle/Controller/QuarkLiberteController.php
@@ -0,0 +1,29 @@
+<?php
+
+
+namespace AppBundle\Controller;
+
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Class DefaultController
+ * @package AppBundle\Controller
+ */
+class QuarkLiberteController extends Controller
+{
+
+    /**
+     * Index action
+     *
+     * @Route("/liberte", name="liberte")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function indexAction(Request $request)
+    {
+        return $this->render('default/liberte.html.twig');
+    }
+}
